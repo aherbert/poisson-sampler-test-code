65c962c5b1de1cb8c3ae3b69e3682fc9a3a5af26
aherbert <a.herbert@sussex.ac.uk>
2018-08-03 13:25:38 +0100

    Allow parsing the PoissonSamplerCache benchmark results

--- results/parse_jmh_report.pl
+++ results/parse_jmh_report.pl
@@ -7,8 +7,8 @@ my $prog = basename($0);
 
 my $usage = "
   Program to parse the CSV output from the JMH benchmark and produce a table
-  for insertion into JIRA
-
+  for insertion into JIRA.
+  
 Usage:
 
   $prog input [...]
@@ -31,93 +31,155 @@ die $usage if $help;
 # Delete as necessary
 my $input = shift or die $usage;
 
+sub find($@) {
+    local $target = "Param: " . shift;
+    $i = 0;
+    for (@_) {
+        return $i if m/$target/;
+        $i++;
+    }
+    warn "Failed to find columns for $target\n";
+    return -1;
+}
+
+open (IN, $input) or die "Failed to open '$input': $!\n";
+$header = readline(IN);
+close IN;
+
 # Find columns of interest
 $iname = 0;
 $iscore = 4;
-$imean = 7;
-$isource = 8;
+@cols = split /,/, $header;
+$imean = find("mean", @cols);
+$isource = find("randomSourceName", @cols);
+$irange = find("range", @cols);
 
-open (IN, $input) or die "Failed to open '$input': $!\n";
-my $header = readline(IN);
+die if $isource == -1;
 
-while (<IN>)
-{
-    chomp;
-    s/"//g;
-    s/^.*(Repeat|Single)Use_//;
-    $bench = $1;
-    @cols = split /,/, $_;
-    
-    $sources{$cols[$isource]} = 1;
-    $means{$cols[$imean]} = 1;
-    $data{$cols[$isource]}{$bench}{$cols[$imean]}{$cols[$iname]} = $cols[$iscore];
-}
-close IN;
+# Single vs repeat use
+if ($imean != -1) {
 
-print "||Source||Use||Mean||Name||Relative Score||\n";
-for $source (sort keys %sources)
-{
-    for $bench (sort keys %{$data{$source}}) 
+    open (IN, $input) or die "Failed to open '$input': $!\n";
+    $header = readline(IN);
+    while (<IN>)
+    {
+        chomp;
+        s/"//g;
+        s/^.*(Repeat|Single)Use_//;
+        $bench = $1;
+        @cols = split /,/, $_;
+        
+        $sources{$cols[$isource]} = 1;
+        $means{$cols[$imean]} = 1;
+        $data{$cols[$isource]}{$bench}{$cols[$imean]}{$cols[$iname]} = $cols[$iscore];
+    }
+
+    print "||Source||Use||Mean||Name||Relative Score||\n";
+    for $source (sort keys %sources)
+    {
+        for $bench (sort keys %{$data{$source}}) 
+        {
+            for $mean (sort {$a<=>$b} keys %means)
+            {
+                $first = 0;
+                for $name (sort {
+                        $data{$source}{$bench}{$mean}{$b} <=> $data{$source}{$bench}{$mean}{$a}
+                    } keys %{$data{$source}{$bench}{$mean}}) 
+                {
+                    $score = $data{$source}{$bench}{$mean}{$name};
+                    if ($first) {
+                        $rel = $score / $first;
+                    } else {
+                        $rel = 1;
+                        $first = $score;
+                    }
+                    print "|$source|$bench|$mean|$name|$rel|\n";
+                }
+            }
+        }
+    }
+
+    # Do it again to compare single & repeats use
+    open (IN, $input) or die "Failed to open '$input': $!\n";
+    $header = readline(IN);
+
+    my %data;
+
+    while (<IN>)
+    {
+        chomp;
+        s/"//g;
+        s/^.*(Repeat|Single)Use_/$1 /;
+        @cols = split /,/, $_;
+        
+        $score = ($1 eq 'Repeat') ? $cols[$iscore] : $cols[$iscore] * 10;
+        $data{$cols[$isource]}{$cols[$imean]}{$cols[$iname]} = $score;
+    }
+    close IN;
+
+    print "||Source||Mean||Name||Relative Score||\n";
+    for $source (sort keys %sources)
     {
         for $mean (sort {$a<=>$b} keys %means)
         {
-            $first = 0;
+            # Do relative to the original
+            $first = $data{$source}{$mean}{'Repeat PoissonSampler'};
             for $name (sort {
-                    $data{$source}{$bench}{$mean}{$b} <=> $data{$source}{$bench}{$mean}{$a}
-                } keys %{$data{$source}{$bench}{$mean}}) 
+                    $data{$source}{$mean}{$b} <=> $data{$source}{$mean}{$a}
+                } keys %{$data{$source}{$mean}}) 
             {
-                $score = $data{$source}{$bench}{$mean}{$name};
+                $score = $data{$source}{$mean}{$name};
                 if ($first) {
                     $rel = $score / $first;
                 } else {
                     $rel = 1;
                     $first = $score;
                 }
-                print "|$source|$bench|$mean|$name|$rel|\n";
+                print "|$source|$mean|$name|$rel|\n";
             }
         }
     }
-}
 
-# Do it again to compare single & repeats use
-open (IN, $input) or die "Failed to open '$input': $!\n";
-my $header = readline(IN);
+} 
 
-my %data;
+# Single vs repeat use
+if ($irange != -1) {
 
-while (<IN>)
-{
-    chomp;
-    s/"//g;
-    s/^.*(Repeat|Single)Use_/$1 /;
-    @cols = split /,/, $_;
-    
-    $score = ($1 eq 'Repeat') ? $cols[$iscore] : $cols[$iscore] * 10;
-    $data{$cols[$isource]}{$cols[$imean]}{$cols[$iname]} = $score;
-}
-close IN;
-
-print "||Source||Mean||Name||Relative Score||\n";
-for $source (sort keys %sources)
-{
-    for $mean (sort {$a<=>$b} keys %means)
+    open (IN, $input) or die "Failed to open '$input': $!\n";
+    $header = readline(IN);
+    my %data;
+    while (<IN>)
     {
-        # Do relative to the original
-        $first = $data{$source}{$mean}{'Repeat PoissonSampler'};
-        for $name (sort {
-                $data{$source}{$mean}{$b} <=> $data{$source}{$mean}{$a}
-            } keys %{$data{$source}{$mean}}) 
+        chomp;
+        s/"//g;
+        s/^.*runPoissonSamplerCache_//;
+        @cols = split /,/, $_;
+        
+        $sources{$cols[$isource]} = 1;
+        $ranges{$cols[$irange]} = 1;
+        $data{$cols[$isource]}{$cols[$irange]}{$cols[$iname]} = $cols[$iscore];
+    }
+
+    print "||Source||Range||Name||Relative Score||\n";
+    for $source (sort keys %sources)
+    {
+        for $range (sort {$a<=>$b} keys %ranges)
         {
-            $score = $data{$source}{$mean}{$name};
-            if ($first) {
-                $rel = $score / $first;
-            } else {
-                $rel = 1;
-                $first = $score;
+            $first = 0;
+            for $name (sort {
+                    $data{$source}{$range}{$b} <=> $data{$source}{$range}{$a}
+                } keys %{$data{$source}{$range}}) 
+            {
+                $score = $data{$source}{$range}{$name};
+                if ($first) {
+                    $rel = $score / $first;
+                } else {
+                    $rel = 1;
+                    $first = $score;
+                }
+                print "|$source|$range|$name|$rel|\n";
             }
-            print "|$source|$mean|$name|$rel|\n";
         }
     }
 }
-
 
